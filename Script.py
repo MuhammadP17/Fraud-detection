747fea3db7acb2dba1e3261afc67e05f71476db8
MuhammadP17 <basef973@gmail.com>
2025-12-05 15:22:23 +0200

    Initial Setup

--- Script.py
+++ Script.py
@@ -1,13 +1,45 @@
+import os
+
 import pandas as pd
 import numpy as np
+import matplotlib
+if 'PYCHARM_HOSTED' in os.environ:
+    matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import seaborn as sns
+import warnings
+warnings.filterwarnings('ignore')
 
 
 
-#Import dataset
+#Import dataset and check properties.
 df = pd.read_csv('Data.zip', compression='zip')
 print(df.head())
 print(df.describe())
+print(df.info())
+
+#Check for null values
+print(df.isnull().sum().max())
+
+
+#Check dataset for imbalance
+sns.countplot(x='Class', data=df, palette='Set3')
+plt.title("Count by Class (Fraud = 1)")
+plt.show()
+
+
+#Check for and remove outliers.
+
+
+#Need to make the dataset balanced to make an accurate enough model.
+
+
+#Also need to remove outliers for better model efficiency.
+
+#Then need to split the data into a training and testing set respectively.
+
+#Correlation matrix with subsample to check for relationships between variables.
+
+#And some more stuff.
 
 
