83f69c8fc49fc8b79afb721045c110dbf4d9a3d1
MuhammadP17 <basef973@gmail.com>
2025-12-05 12:08:05 +0200

    Initial Setup

--- Script.py
+++ Script.py
@@ -1,1 +1,13 @@
-print("Project initialized!")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+#Import dataset
+df = pd.read_csv('Data.zip', compression='zip')
+print(df.head())
+print(df.describe())
+
+
